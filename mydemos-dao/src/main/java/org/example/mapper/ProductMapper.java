70aaee7a696c07b9c1845336555388780dbdb9a6
芭比Q拉兄弟们 <91517385+AC0000001@users.noreply.github.com>
2023-06-12 15:34:17 +0800

    Initial commit

---
+++ mydemos-dao/src/main/java/org/example/mapper/ProductMapper.java
@@ -0,0 +1,19 @@
+package org.example.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.example.entity.Product.Product;
+
+/**
+ * @Author: Shengke
+ * @Date: 2023-06-09-10:45
+ * @Description:
+ */
+@Mapper
+public interface ProductMapper extends BaseMapper<Product> {
+
+
+    Product selectByName(@Param("productName") String title);
+}
---
+++ mydemos-service/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+//package org.example;
+//
+///**
+// * @Author: Shengke
+// * @Date: 2023-06-12-15:22
+// * @Description:
+// */
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//    }
+//}
